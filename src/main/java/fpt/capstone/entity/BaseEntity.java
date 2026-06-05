eaf582b4814be5e4226fd9cd0c8c32afe90e56b8
TuTM <trolling1349765@gmail.com>
2026-06-05 10:12:35 +0700

    add field

--- src/main/java/fpt/capstone/entity/BaseEntity.java
+++ src/main/java/fpt/capstone/entity/BaseEntity.java
@@ -1,10 +1,27 @@
 package fpt.capstone.entity;
 
+import jakarta.persistence.Column;
+
 import java.util.Date;
+
 
 public class BaseEntity {
 
-    default Date createDate;
-    default Date updateDate;
-    default boolean isDelete;
+    @Column(name = "Create_At")
+    protected Date createAt;
+
+    @Column(name = "Create_By")
+    protected String createBy;
+
+    @Column(name = "Update_At")
+    protected Date updateAt;
+
+    @Column(name = "Update_By")
+    protected String updateBy;
+
+    @Column(name = "Is_Delete")
+    protected boolean isDelete;
+
+    @Column(name = "Delete_By")
+    protected String deleteBy;
 }
