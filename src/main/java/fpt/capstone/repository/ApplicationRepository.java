d739b951ea464df0821c12f79356a03d49df97bb
TuTM <trolling1349765@gmail.com>
2026-06-22 21:17:46 +0700

    modify: application

--- src/main/java/fpt/capstone/repository/ApplicationRepository.java
+++ src/main/java/fpt/capstone/repository/ApplicationRepository.java
@@ -1,9 +1,10 @@
 package fpt.capstone.repository;
 
 import fpt.capstone.dto.response.APIResponse;
+import fpt.capstone.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<APIResponse, Integer> {
+public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 }
